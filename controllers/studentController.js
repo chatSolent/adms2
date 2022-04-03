04c2bd382f58f8527d0e33fcaf499f387106fa20
chatSolent <66261650+chatSolent@users.noreply.github.com>
2022-04-03 22:26:19 +0100

    registeruser

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,8 +1,12 @@
 require ('../models/database')
 const { name } = require('ejs');
-const Student=require('../models/studentModel')
+const req = require('express/lib/request');
+const { append } = require('express/lib/response');
+const res = require('express/lib/response');
+const Student=require('../models/studentModel');
+const User=require('../models/User')
 
-exports.HomePage= async (req, res)=>{
+exports.HomePage= async (req, res) =>{
     
         const students = await Student.find({})
    
@@ -39,7 +43,7 @@ exports.CreateStudent=(req, res)=>{
 }
 
 // Edit Student
-exports.EditStudent= async (req, res)=>{
+exports.UpdateStudentPage= async (req, res)=>{
     console.log(req.params.id);
     const id = req.params.id;
     const student = await Student.findById({_id:id})
@@ -47,11 +51,8 @@ exports.EditStudent= async (req, res)=>{
 
 }
 // Edit Student Action
-exports.EditStudentAction=async (req, res)=>{
-    //console.log(req.params.id);
-    //const id=req.params.id
-    //let name =req.body.name
-    //let email =req.body.email
+exports.UpdateStudent=async (req, res)=>{
+   
    try {
     const student = await Student.updateOne({_id:req.params.id, name:req.body.name, email:req.body.email})
     res.redirect('/')
@@ -70,3 +71,32 @@ exports.DeleteStudent=async(req, res)=>{
     console.log(student);
     res.redirect('/');
 }
+
+//
+
+// create form view
+exports.RegisterPage=(req, res)=>{
+    res.render('register');
+}
+//User
+exports.RegisterUser=(req,res)=>{
+    console.log(req.body)
+    User.findOne({email:req.body.email}).then((User)); {
+    if (User){
+
+        return res.status(400).json({email: "A user already registered"})
+    }else {
+// or create new user 
+        const newUser =new User({
+           userName:req.body.name,
+           email:req.body.email,
+           password: req.body.password,
+
+        });
+            newUser.save()
+            return res.status(200).json({msg: newUser})
+        }
+    };
+};
+
+
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -7,13 +7,22 @@ const studentController = require('../controllers/studentController')
 router.get('/', studentController.HomePage);
 router.get('/create_student', studentController.CreatePage);
 router.post('/create_student', studentController.CreateStudent);
+
 // Update
 router.get('/', studentController.HomePage);
-router.get('/edit_student/:id', studentController.EditStudent);//router.get('/edit_student/:id', studentController.EditStudet);
-router.post('/edit_student/:id', studentController.EditStudentAction);
-//router.delete(('/delete_student/:id',function(req, res))
-module.exports=router
+router.get('/edit_student/:id', studentController.UpdateStudentPage);//router.get('/edit_student/:id', studentController.EditStudet);
+router.post('/edit_student/:id', studentController.UpdateStudent);
+
+
 
 //Delete
 router.get('/delete_student/:id', studentController.DeleteStudent);
-//router.delete('/delete_student/:id',studentController.DeleteStudent) 
+
+//register
+
+//router.get('/', studentController.HomePage);
+router.get('/register', studentController.RegisterPage);
+router.post('/register', studentController.RegisterUser);
+
+
+module.exports=router
